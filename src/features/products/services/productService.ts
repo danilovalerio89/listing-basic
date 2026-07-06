385d1e27e08db3360d8de6f0efe056904051d2f8
Danilo Valério <danvalerio89@gmail.com>
2026-07-06 15:51:38 -0300

    chore: add product mock and types

---
+++ src/features/products/services/productService.ts
@@ -0,0 +1,21 @@
+import type { Product } from "@/features/products/types";
+
+const mockProducts: Product[] = [
+  { id: 1, name: "Laptop", category: "Electronics", price: 1 },
+  { id: 2, name: "Coffee Maker", category: "Home Appliances", price: 2 },
+  { id: 3, name: "Desk Chair", category: "Furniture", price: 3 },
+  { id: 4, name: "Smartphone", category: "Electronics", price: 4 },
+  { id: 5, name: "Headphones", category: "Accessories", price: 5 },
+  { id: 11, name: "Laptop", category: "Electronics", price: 6 },
+  { id: 21, name: "Coffee Maker", category: "Home Appliances", price: 7 },
+  { id: 31, name: "Desk Chair", category: "Furniture", price: 8 },
+  { id: 41, name: "Smartphone", category: "Electronics", price: 9 },
+  { id: 51, name: "Headphones", category: "Accessories", price: 10 },
+  { id: 12, name: "Laptop", category: "Electronics", price: 11 },
+  { id: 22, name: "Coffee Maker", category: "Home Appliances", price: 12 },
+  { id: 32, name: "Desk Chair", category: "Furniture", price: 13 },
+  { id: 42, name: "Smartphone", category: "Electronics", price: 14 },
+  { id: 52, name: "Headphones", category: "Accessories", price: 15 },
+];
+
+export default mockProducts;
